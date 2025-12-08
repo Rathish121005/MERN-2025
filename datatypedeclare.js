015b3f02ad6ca5ac454aa6690d1e8a55ccf15374
Rathish121005 <aarockyarathishraj2005@gmail.com>
2025-12-08 09:20:55 +0530

    added

---
+++ datatypedeclare.js
@@ -0,0 +1,8 @@
+a=10
+b="sece"
+c=12.10
+d=a+b
+e=true
+console.log(d);
+console.log(a+b);
+console.log(a+b+e);
---
+++ index.js
@@ -0,0 +1,34 @@
+var a;//global scope
+a=10;
+var a=20;//redeclared
+let b;//block scope
+const c=12;//constant value
+b=10;
+b=11;
+
+//Data Types in JS
+var h=20;//number
+var d="ARRaj";//string
+var e=true;//boolean
+var f=null;//null
+var g=undefined;//undefined
+
+
+var object={
+    name:"ARRaj",
+    age:20,
+    phone:9042915939,
+    email:"aarockyarathishraj2005@gmail.com",
+    address:{
+        houseno:"1/284A",
+        street:"Middle Street",
+        city:"Silaiman",
+        district:"Madurai",
+        pincode:625201,
+    }
+}
+console.log(object);
+
+
+
+
